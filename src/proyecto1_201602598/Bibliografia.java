e43e27094e958c468e97df0da69de82d9244b8a3
Hector <Hector@192.168.0.13>
2017-09-26 23:58:22 -0600

    Practica1_201602598

--- src/proyecto1_201602598/Bibliografia.java
+++ src/proyecto1_201602598/Bibliografia.java
@@ -3,14 +3,17 @@ package proyecto1_201602598;
 
 public class Bibliografia {
 
-private static String autor;
+    private static String[] bibliografia=new String[600];
+    private static int longibibliografia=0;
+    
+/*private static String autor;
 private static String titulo;
 private static int edicion;
 private static String[] palcla;
 private static String descripcion;
 private static String[] temas;
 private static String copias;
-private static int disponibles;
+private static int disponibles;*/
 
 /*private static String frecuenciaactual;
 private static int ejemplares;
@@ -18,15 +21,22 @@ private static String area;*/
 
 public Bibliografia(){
     
-    autor="";
+    /*autor="";
     titulo="";
     edicion=0;
     palcla=new String[500];
     descripcion="";
     temas=new String[500];
     copias="";
-    disponibles=0;
+    disponibles=0;*/
     
+}
+
+public void setBibliografia(String autor,String titulo,int edicion,String[] palcla,String descripcion,
+        String[] temas,String copias,int disponibles){
+    
+    bibliografia[longibibliografia]=autor+";"+titulo+";"+edicion+";"+palcla+";"+descripcion+";"+temas
+            +";"+copias+";"+disponibles;
 }
 
     
